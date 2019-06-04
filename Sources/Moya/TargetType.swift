5416e03cecd002cb251233431685bf758ab5b112
dangthaison91 <dangthaison.91@gmail.com>
2019-06-04 15:01:14 +0700

    Support url parameters

--- Sources/Moya/TargetType.swift
+++ Sources/Moya/TargetType.swift
@@ -23,6 +23,9 @@ public protocol TargetType {
 
     /// The headers to be used in the request.
     var headers: [String: String]? { get }
+    
+    /// The query parameters to be used in the request.
+    var urlParameters: [String: Any]? { get }
 }
 
 public extension TargetType {
